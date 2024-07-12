967b0e08c173d616e0a3c96068d4bf93e9e8c038
fxzer <fxzer8888@163.com>
2024-07-12 10:48:26 +0800

    feat: 返回已存在文件URL

--- client/src/components/LargeFileUpload.vue
+++ client/src/components/LargeFileUpload.vue
@@ -1,6 +1,6 @@
 <script setup lang='ts'>
 import axios from 'axios'
-import type { UploadRequestOptions } from 'element-plus'
+// import type { UploadRequestOptions } from 'element-plus'
 
 const API = {
   check: '/check',
@@ -28,41 +28,57 @@ interface Chunk {
   size: string
 }
 
-async function handleUpload(options: UploadRequestOptions) {
-  const file = options.file
-  if (file) {
-    const chunks = createChunks(file)
-    const pickedChunks = pickChunks(chunks, CHUNK_SIZE)
-    // const hash = await calculateHash(chunks)
-    // 创建一个Worker实例
-    const worker = new Worker('hashWorker.js')
+const inputRef = ref<HTMLInputElement | null>(null)
+const videoRef = ref<HTMLVideoElement | null>(null)
+// async function handleUpload(options: UploadRequestOptions) {
+//   const file = options.file
+async function handleUpload(e: Event) {
+  // const file = inputRef.value?.files?.[0]
+  const file = (e.target as HTMLInputElement).files?.[0]
+  if (!file) {
+    ElNotification({
+      title: '请选择文件',
+      type: 'warning',
+    })
+    return
+  }
+  const chunks = createChunks(file)
+  const pickedChunks = pickChunks(chunks, CHUNK_SIZE)
+  // const hash = await calculateHash(chunks)
+  // 创建一个Worker实例
+  const worker = new Worker('hashWorker.js')
 
-    // 向Worker发送数据
-    worker.postMessage({ chunks: pickedChunks })
+  // 向Worker发送数据
+  worker.postMessage({ chunks: pickedChunks })
 
-    // 监听Worker返回的数据
-    let fileHash = ''
-    worker.onmessage = async function (event) {
-      const { progress, hash } = event.data
-      if (progress) {
-        progressMap.calculateHash = progress
-      }
-      if (hash) {
-        fileHash = hash
-        worker.terminate()
-        const result = await checkExist(fileHash)
-        if (result.uploaded) {
-          ElNotification({
-            title: result.message,
-            type: 'warning',
-          })
-        }
-        else {
-          // 文件不存在 , 上传文件
-          await uploadChunks(chunks, fileHash)
-          console.log('[ chunks ]-63', chunks)
+  // 监听Worker返回的数据
+  let fileHash = ''
+  worker.onmessage = async function (event) {
+    const { progress, hash } = event.data
+    if (progress) {
+      progressMap.calculateHash = progress
+    }
+    if (hash) {
+      fileHash = hash
+      worker.terminate()
+      const { shouldUpload, message, uploadedChunks = [], fileUrl } = await checkExist(fileHash)
+      if (shouldUpload) {
+      // 需要上传, 则上传文件 ，分片都已上传，则直接合并
+        if (uploadedChunks.length === chunks.length) {
           await mergeRequest(file.name, CHUNK_SIZE, fileHash)
         }
+        else {
+          await uploadChunks(chunks, fileHash, uploadedChunks)
+          await mergeRequest(file.name, CHUNK_SIZE, fileHash)
+        }
+      }
+      else {
+        ElNotification({
+          title: message,
+          type: 'warning',
+        })
+        if (videoRef.value && fileUrl)
+          videoRef.value.src = fileUrl
       }
     }
   }
@@ -110,8 +126,9 @@ function pickChunks(chunks: Chunk[], chunkSize: number) {
 function checkExist(fileHash: string) {
   return axios.post(`/api${API.check}`, { fileHash }).then(res => res.data)
 }
-async function uploadChunks(chunks: Chunk[], hash: string) {
-  const formDatas = chunks.map(({ file, name }) => {
+
+async function uploadChunks(chunks: Chunk[], hash: string, uploadedChunks: number[] = []) {
+  const formDatas = chunks.filter((_, index) => !uploadedChunks.includes(index)).map(({ file, name }) => {
     const formData = new FormData()
     formData.append('chunk', file)
     formData.append('chunkName', name)
@@ -132,7 +149,9 @@ async function currencyUpload(formDatas: FormData[]) {
     // const task = axios.post(`/api${API.upload}`, formDatas[index], {
     //   onUploadProgress: (progress) => {
     //     allProgress += (progress.loaded / (progress.total ? progress.total : 1)) // 这是单个分片的
+    //     console.log('[ allProgress ]-152', allProgress)
     //     progressMap.uploadChunks = (allProgress / formDatas.length) * 100
+    //     console.log('[ progressMap.uploadChunks  ]-154', progressMap.uploadChunks)
     //     // if (params.onProgress) params.onProgress({ percent })
     //   },
     // })
@@ -157,22 +176,28 @@ async function currencyUpload(formDatas: FormData[]) {
 }
 // 合并请求
 async function mergeRequest(fileName: string, chunkSize: number, fileHash: string) {
-  const { data } = await axios.post(`/api${API.merge}`, { chunkSize, fileHash, fileName })
-  if (data.success) {
+  const { success, message, fileUrl } = await axios.post(`/api${API.merge}`, { chunkSize, fileHash, fileName }).then(res => res.data)
+  console.log('[ fileUrl ]-180', fileUrl)
+
+  if (success) {
     ElNotification({
-      title: data.message,
+      title: message,
       type: 'success',
     })
+    if (videoRef.value)
+      videoRef.value.src = fileUrl
   }
 }
 </script>
 
 <template>
   <div class="mxa w-8/10">
-    <!-- <input type="file" @input="handleUpload">
-     -->
-
-    <el-upload drag :http-request="handleUpload">
+    <input ref="inputRef" type="file" @input="handleUpload">
+    <button btn @click="handleUpload">
+      上传
+    </button>
+    <video ref="videoRef" mt-5 mxa w-200 src="" muted controls />
+    <!-- <el-upload drag :http-request="handleUpload">
       <div class="el-upload__text h-30 flex-center">
         拖拽文件到此 <em> 或点击上传</em>
       </div>
@@ -191,7 +216,7 @@ async function mergeRequest(fileName: string, chunkSize: number, fileHash: strin
           </p>
         </div>
       </template>
-    </el-upload>
+</el-upload> -->
   </div>
 </template>
 
--- server/index.js
+++ server/index.js
@@ -10,18 +10,67 @@ const API = {
   upload: '/upload',
   merge: '/merge',
 }
+const SERVER_URL = 'http://localhost:8001'
+const UPLOAD_INFO = {
+  FAIL: { success: false, message: '上传失败' },
+  SUCCESS: { success: true, message: '上传成功' },
+  EXIST: { shouldUpload: false, message: '文件已存在' },
+  NOT_EXIST: { shouldUpload: true, message: '文件不存在', uploadedChunks: [] },
+  NO_CHUNK_DIR: { message: '分片文件夹不存在', success: false },
+  MERGE_SUCCESS: { message: '合并成功', success: true },
+}
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
+// 托管静态文件 upload 目录
+app.use('/upload', express.static('./upload'))
 
 const UPLOAD_DIR = path.resolve(__dirname, './upload')
+
+// 检查文件是否存在
+app.post(API.check, async (req, res) => {
+  const { fileHash } = req.body
+  // upload 目录不存在
+  if (!fse.existsSync(UPLOAD_DIR)) {
+    res.status(200).json(UPLOAD_INFO.NOT_EXIST)
+    return
+  }
+
+  // 合并后的文件已存在： 已上传的文件名中是否包含当前 hash
+  const uploadedFiles = await fse.readdir(UPLOAD_DIR).then((fileOrDirs) => {
+    return fileOrDirs.filter((item) => {
+      return fse.statSync(path.resolve(UPLOAD_DIR, item)).isFile() && !item.startsWith('.')
+    })
+  })
+  const isExist = uploadedFiles.some((fileName) => {
+    if (fileName.includes(fileHash)) {
+      res.status(200).json({ ...UPLOAD_INFO.EXIST, fileUrl: `${SERVER_URL}/upload/${fileName}` })
+      return true
+    }
+    return false
+  })
+  if (!isExist) {
+    // 分片临时目录已存在
+    const chunkDir = path.resolve(UPLOAD_DIR, fileHash)
+    if (fse.existsSync(chunkDir)) {
+      // 返回已上传的分片列表
+      const uploadedChunks = await fse.readdir(chunkDir).then((chunkNames) => {
+        return chunkNames.filter(chunkName => !chunkName.startsWith('.')).map(chunkName => +chunkName.split('-')[0])
+      })
+      res.status(200).json({ ...UPLOAD_INFO.NOT_EXIST, uploadedChunks })
+    }
+    else {
+      res.status(200).json(UPLOAD_INFO.NOT_EXIST)
+    }
+  }
+})
 
 app.post(API.upload, (req, res) => {
   const form = new multipart.Form()
   form.parse(req, async (err, fields, files) => {
     if (err) {
-      res.status(401).json({ message: '上传失败' })
+      res.status(401).json(UPLOAD_DIR.FAIL)
       return
     }
     const chunkName = fields.chunkName[0]
@@ -34,31 +83,8 @@ app.post(API.upload, (req, res) => {
       await fse.mkdirs(chunkDir)
     }
     await fse.move(chunk.path, `${chunkDir}/${chunkName}`)
-    res.status(200).json({ message: '上传成功', success: true })
+    res.status(200).json(UPLOAD_DIR.success)
   })
-})
-
-// 检查文件是否存在
-app.post(API.check, async (req, res) => {
-  const { fileHash } = req.body
-  // upload 目录不存在
-  if (!fse.existsSync(UPLOAD_DIR)) {
-    res.status(200).json({ uploaded: false, message: '文件不存在' })
-    return
-  }
-  // 已上传的文件名中是否包含当前 hash
-  const uploadedFiles = await fse.readdir(UPLOAD_DIR).then((fileOrDir) => {
-    return fileOrDir.filter((item) => {
-      return fse.statSync(path.resolve(UPLOAD_DIR, item)).isFile()
-    })
-  })
-  const isExist = uploadedFiles.some(fileName => fileName.includes(fileHash))
-  if (isExist) {
-    res.status(200).json({ uploaded: true, message: '文件已存在' })
-  }
-  else {
-    res.status(200).json({ uploaded: false, message: '文件不存在' })
-  }
 })
 
 // 合并分片
@@ -66,13 +92,13 @@ app.post(API.merge, async (req, res) => {
   const { fileHash, fileName, chunkSize } = req.body
   const chunkDir = path.resolve(UPLOAD_DIR, fileHash)
   if (!fse.existsSync(chunkDir)) {
-    res.status(401).json({ message: '分片文件夹不存在' })
+    res.status(401).json(UPLOAD_INFO.NO_CHUNK_DIR)
     return
   }
 
   // 合并分片
   const chunkNames = await fse.readdir(chunkDir)
-  chunkNames.sort((a, b) => a.split('-')[0] - b.split('-')[0])
+  chunkNames.sort((a, b) => +a.split('-')[0] - +b.split('-')[0])
   const mergePromises = chunkNames.map((chunkName, index) => new Promise((resolve) => {
     const chunkPath = path.resolve(chunkDir, chunkName)
     const readStream = fse.createReadStream(chunkPath)
@@ -90,9 +116,9 @@ app.post(API.merge, async (req, res) => {
   }))
   await Promise.all(mergePromises)
   fse.remove(chunkDir)
-  res.json({ message: '合并成功', success: true })
+  res.json({ ...UPLOAD_DIR.MERGE_SUCCESS, fileUrl: `${SERVER_URL}/upload/${fileHash}-${fileName}` })
 })
 
 app.listen(8001, () => {
-  console.warn('server is running on http://localhost:8001')
+  console.warn(`server is running on ${SERVER_URL}`)
 })
